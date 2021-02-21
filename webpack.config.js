2d8c566503e1c8e9da960d4fbc84d34e6b41847d
Sonata Katt <sonata.abdullh@gmail.com>
2021-02-21 15:50:52 -0500

    fix failed depl

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index_bundle.js'
